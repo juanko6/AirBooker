58c28c0cf562ecae77ed4c03dce75a78e8324edf
raul <rca63@alu.ua.es>
2025-02-17 09:12:29 +0000

    modelos hechos

---
+++ airbooker/database/migrations/2025_02_17_085828_cliente.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('telefono', '15');
+            $table->string('dni', '9');
+            $table->string('pasaporte');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
