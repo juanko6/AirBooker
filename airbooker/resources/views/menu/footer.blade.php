c2e35ac63ae7f26d33e6a9e6825f2d796e097790
Naoufal <nc25@alu.ua.es>
2025-03-26 09:23:17 +0100

    AJUSTES EN EL FOOTER

--- airbooker/resources/views/menu/footer.blade.php
+++ airbooker/resources/views/menu/footer.blade.php
@@ -94,22 +94,13 @@
             <div class="copyright-area">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-6 col-lg-6 text-center text-lg-left">
-                            <div class="copyright-text">
-                                <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
+                        <div class="text-center ">
+                            <div class="copyright-text text-center">
+                                <p>Copyright &copy; 2024, <a href="http://localhost:8000/">AIRBOOKER</a></p>
+                            </div>
                             </div>
                         </div>
-                        <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                            <div class="footer-menu">
-                                <ul>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Terms</a></li>
-                                    <li><a href="#">Privacy</a></li>
-                                    <li><a href="#">Policy</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
