c20d51bd338b76402e15cffbac5d319ba5523c83
Juanko6 <juanko6@gmail.com>
2025-03-26 12:12:40 +0100

    modificacion de estilos para que la paginacion se mostrara bien en todas las opciones

--- airbooker/resources/views/menu/header.blade.php
+++ airbooker/resources/views/menu/header.blade.php
@@ -22,7 +22,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-
+    <script src="{{ asset('js/listas.js') }}"></script>
     <script src="{{ asset('js/header.js') }}"></script>
 </head>
 <body>   
